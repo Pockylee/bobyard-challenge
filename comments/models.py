d8120a626653fd75edc19a9a409054dcd582d156
Brian <45988014+Pockylee@users.noreply.github.com>
2025-09-19 14:15:20 -0700

    first commit

---
+++ comments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Commens model
+class Comment(models.Model):
+    author = models.CharField(max_length = 50)
+    text = models.TextField()
+    date = models.DateTimeField()
+    likes = models.IntegerField(default = 0)
+    image = models.URLField(blank = True)
+
+    # String representation of the django admin to keep clean.
+    def __str__(self):
+        return f"{self.author}: {self.text[:40]}..."
