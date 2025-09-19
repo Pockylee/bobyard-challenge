1be0f9f358156d3828fa1c8237331d22d5c9f00d
Brian <45988014+Pockylee@users.noreply.github.com>
2025-09-19 14:41:21 -0700

    feat: integrate DefaultRouter for comments API endpoints

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from comments.views import CommentViewSet
+
+router = DefaultRouter()
+router.register(r'comments', CommentViewSet, basename = "comments")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
