ca774ca3713c0f57ff790539ea4b4577507ee2d7
Brian <45988014+Pockylee@users.noreply.github.com>
2025-09-19 14:37:28 -0700

    feat: add CommentViewSet for managing comments with CRUD operations

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CommentSerializer
+from .models import Comment
 
-# Create your views here.
+# CommentViewSet which provide GET, POST, GET, PUT, DELETE
+class CommentViewSet(viewsets.ModelViewSet):
+    
+    queryset = Comment.objects.all().order_by("-date")
+    serializer_class = CommentSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["date", "likes"] # Consider date and likes as the most important fields to order by in current webstie
+    
