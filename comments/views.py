ab771f1e7e84c4e6bebabea5593c22d777915eab
Brian <45988014+Pockylee@users.noreply.github.com>
2025-09-19 14:46:41 -0700

    fix: not importing packages in urls and views file

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import filters
 from .serializers import CommentSerializer
 from .models import Comment
 
